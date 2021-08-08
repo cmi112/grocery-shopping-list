8f2ddb232bde6a18c7273aa7e89c208b1f44547d
cmi112 <cabdiqanimuuse7@gmail.com>
2021-08-09 01:48:54 +0200

    Version 1.0.0

--- client/src/config/baseURL.js
+++ client/src/config/baseURL.js
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.REACT_APP_ENV === "development"
     ? "http://localhost:5000/items/v1"
-    : "https://pure-lowlands-97904.herokuapp.com";
+    : "https://github.com/cmi112/grocery-shopping-list.git";
 
 export default baseURL;
