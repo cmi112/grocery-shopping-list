9fccaf9c06d560d7b97027f1ef7742d12e5dce8b
cmi112 <cabdiqanimuuse7@gmail.com>
2021-08-09 02:52:45 +0200

    Version 1.0.4

--- client/src/config/baseURL.js
+++ client/src/config/baseURL.js
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.REACT_APP_ENV === "development"
-    ? "http://localhost:5000/items/v1"
+    ? "http://localhost:5000/"
     : "https://github.com/cmi112/grocery-shopping-list.git";
 
 export default baseURL;
